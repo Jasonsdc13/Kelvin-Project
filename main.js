4787d268068484e23753ea95f859ce2f5926a1db
Jasonsdc13 <jasons_dc13@yahoo.com>
2024-07-28 01:11:20 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+// The forecats today is 293 kelvin
+const kelvin = 0;
+
+// celsius is 273 degrees less than kelvin
+var celsius = (kelvin - 273);
+
+// We are conveting celsius to fahrenheit
+var fahrenheit = celsius * (9/5) + 32;
+
+// We are rounding fahrenheit to the lowest whole number
+Math.floor(fahrenheit);
+
+var temperature = fahrenheit;
+console.log(`The temperature is ${temperature} degrees Fahrenheit.`);
+
+// I'm converting celsius to newton in the following lines
+
+// var newton = (celsius * (33/100));
+
+// Math.floor(newton);
+
+// var temperature = newton;
+// console.log(`The temperature is ${temperature} degrees newton.`);
